794ecb75412157e1c6514661063d9233292ee387
jesusudiz <jesusgabrieludiz@gmail.com>
2022-09-13 14:47:25 -0300

    se realizo el ejercicio del module 04

--- code/module-04/m04-end/module04.ts
+++ code/module-04/m04-end/module04.ts
@@ -40,27 +40,26 @@ let sortAscending: compareFunctionType = (a, b) => {
 /*  TODO: Update the BuildArray function. */
 
 function buildArray(items: number, sortOrder: 'ascending' | 'descending'): number[] {
-    let randomNumbers: number[] = [];
-    let nextNumber: number;
-    for (let counter = 0; counter < items; counter++) {
-        nextNumber = Math.ceil(Math.random() * (100 - 1));
-        if (randomNumbers.indexOf(nextNumber) === -1) {
-          randomNumbers.push(nextNumber);
-        } else {
-          counter--;
-        }
-    }
-    if (sortOrder === 'ascending') {
-      return randomNumbers.sort(sortAscending);
-    } else {
-      return randomNumbers.sort(sortDescending);
-    }
+  let randomNumbers: number[] = [];
+  let nextNumber: number;
+
+  for (let counter = 0; counter < items; counter++) {
+      nextNumber = Math.ceil(Math.random() * (100 - 1));
+      if (randomNumbers.indexOf(nextNumber) === -1) {
+        randomNumbers.push(nextNumber);
+      } else {
+        counter--;
+      }
+  }
+
+  if (sortOrder === 'ascending') {
+    return randomNumbers.sort(sortAscending);
+  } else {
+    return randomNumbers.sort(sortDescending);
+  }
 }
 
-let myArray1 = buildArray(12, 'ascending');
-let myArray2 = buildArray(8, 'descending');
-console.log(myArray1);
-console.log(myArray2);
+
 
 /*  EXERCISE 2
     TODO: Update the LoanCalculator function. */
